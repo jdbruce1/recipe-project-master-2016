35e5524ca00e416e39bdda4b69397b41d7ddcb04
Jacob Bruce <jacobbruce2016@u.northwestern.edu>
2016-03-01 19:20:14 -0600

    Initial commit
    Changed name and started editing (working with Will)

---
+++ Team2/recipe_api.py
@@ -0,0 +1,21 @@
+'''Version 0.1'''
+
+import urllib2
+
+def autograder(url):
+    '''Accepts the URL for a recipe, and returns a dictionary of the
+    parsed results in the correct format. See project sheet for
+    details on correct format.'''
+    # your code here
+
+    parse('http://allrecipes.com/recipe/44868/spicy-garlic-lime-chicken/?internalSource=popular&referringContentType=home%20page')
+
+    return results
+
+def parse(url):
+	response = urllib2.urlopen(url)
+	html = response.read()
+	print html
+
+
+
